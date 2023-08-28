459d3bdd17994f20f7903bd49c1a1e1b0de7d39e
SeboTimes <sebochrisczemp@gmail.com>
2023-08-28 18:11:44 +0200

    SBinary v0.1

---
+++ SBinary/info.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string name = "SBinary";
+float version = 0.1;
+
+int main() {
+    cout << name << " v" << version << endl;
+    return 0;
+}
